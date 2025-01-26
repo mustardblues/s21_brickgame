b4a1dcc3232d7b310cc82843218d251d4f56382d
mustardblues <manihov.azat@yandex.ru>
2025-01-27 00:43:32 +0300

    done

---
+++ src/brick_game/tetris/tetris.c
@@ -0,0 +1,4 @@
+// Copyright 2025 stranger
+
+#include "tetris.h"
+
---
+++ src/brick_game/tetris/tetris.h
@@ -0,0 +1,59 @@
+// Copyright 2025 stranger
+
+#ifndef _BRICK_GAME_TETRIS_TETRIS_H_
+#define _BRICK_GAME_TETRIS_TETRIS_H_
+
+extern int tetramino_I[4][2] = {
+    {1, 0},
+    {1, 0},
+    {1, 0},
+    {1, 0}
+};
+
+extern int tetramino_J[4][2] = {
+    {0, 1},
+    {0, 1},
+    {1, 1},
+    {0, 0}
+};
+
+extern int tetramino_L[4][2] = {
+    {1, 0},
+    {1, 0},
+    {1, 1},
+    {0, 0}
+};
+
+extern int tetramino_O[4][2] = {
+    {0, 0},
+    {1, 1},
+    {1, 1},
+    {0, 0}
+};
+
+extern int tetramino_S[4][2] = {
+    {1, 0},
+    {1, 1},
+    {0, 1},
+    {0, 0}
+};
+
+extern int tetramino_T[4][2] = {
+    {1, 0},
+    {1, 1},
+    {1, 0},
+    {0, 0}
+};
+
+extern int tetramino_Z[4][2] = {
+    {0, 1},
+    {1, 1},
+    {1, 0},
+    {0, 0}
+};
+
+void start(){
+    
+}
+
+#endif // _BRICK_GAME_TETRIS_TETRIS_H_
